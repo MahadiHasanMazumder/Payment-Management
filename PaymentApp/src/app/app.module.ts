b485e08b597e5d35b38c5d4ac828ec46738ad149
Mahadi <mazumder15-5797@diu.edu.bd>
2021-08-01 16:31:17 +0600

    Implement Insertion

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFromComponent } from './payment-details/payment-detail-from/payment-detail-from.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { PaymentDetailFromComponent } from './payment-details/payment-detail-fro
     PaymentDetailFromComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PaymentApp/src/app/payment-details/payment-detail-from/payment-detail-from.component.ts
+++ PaymentApp/src/app/payment-details/payment-detail-from/payment-detail-from.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-detail-from',
@@ -8,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentDetailFromComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service:PaymentDetailService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form:NgForm)
+  {
+    this.service.postPaymentDetail().subscribe(
+      res=>{
+
+      },
+      err=>{console.log(err);}
+    );
   }
 
 }
